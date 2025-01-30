d9b0da95348cbed502b3aebda208dc31e68a3dc2
Lee Goddard <leegee@gmail.com>
2025-01-30 14:27:30 +0100

    No prepopulation for now

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import './components/TableComponent.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const table = document.querySelector('foo-table');
+// document.addEventListener('DOMContentLoaded', () => {
+    // const table = document.querySelector('foo-table');
     // const data = [
     //     { name: 'Alice', age: 30, location: 'Wonderland' },
     //     { name: 'Bob', age: 25, location: 'Builderland' },
     //     { name: 'Charlie', age: 35, location: 'Chocolate Factory' }
     // ];
     // table.init(data);
+    // 
+    // console.log('Connect to WS', import.meta.env.VITE_WEBSOCKET_URL);
+    // const socket = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL);
 
-    console.log('Connect to WS', import.meta.env.VITE_WEBSOCKET_URL);
-    const socket = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL);
+    // socket.onmessage = (event) => {
+    //     const data = JSON.parse(event.data);
+    //     table.addRow(data);
+    // };
 
-    socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        table.addRow(data);
-    };
-
-});
+// });
